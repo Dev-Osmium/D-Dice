3547e8212e3d7293f757c7f2a3c3bb2ac8613128
Dev-Osmium <Oatmas64134@gmail.com>
2018-08-09 10:34:49 -0800

    Add statroll command group

---
+++ commands/stats/statroll.js
@@ -0,0 +1,25 @@
+const { Command } = require("discord.js-commando");
+const rn = require("random-number");
+
+module.exports = class StatRollCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'statroll',
+            group: 'stats',
+            memberName: 'statroll',
+            description: 'Allows players to quickly perform rolls for stats',
+            examples: ['statroll']
+        });
+    }
+
+    run(message) {
+        var statgen = rn.generator({
+            min: 1,
+            max: 6,
+            integer: true
+        });
+        for (i = 0; i < 4; i++) {
+            var 
+        }
+    }
+}
