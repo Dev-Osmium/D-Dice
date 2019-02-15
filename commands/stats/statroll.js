962f35ac5a68ccd17b0b204cb28e41b05db72d43
Dev-Osmium <devosmium@devosmium.xyz>
2019-02-15 08:52:45 -0500

    Comments for the comment god!

--- commands/stats/statroll.js
+++ commands/stats/statroll.js
@@ -1,4 +1,4 @@
-const { Command } = require("discord.js-commando");
+/*const { Command } = require("discord.js-commando");
 const rn = require("random-number");
 
 module.exports = class StatRollCommand extends Command {
@@ -22,4 +22,4 @@ module.exports = class StatRollCommand extends Command {
             var 
         }
     }
-}
+}*/
